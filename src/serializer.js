3e3d1aa78abd34675ba8cd77f469795af0f0d295
Dylan Foster <dylan947@gmail.com>
2017-10-19 01:48:21 -0700

    docs(serializer): add jsdoc

--- src/serializer.js
+++ src/serializer.js
@@ -1,10 +1,61 @@
 "use strict";
 
 export default class JSONSerializer {
+  /**
+   * Returns an array of Sequelize instances
+   *
+   * @method normalizeArrayResponse
+   * @param {Array} instances Sequelize instances
+   * @return {Promise}<Object, Error>
+   *
+   * @example
+   * ```javascript
+   * return orm.findAll("user").then(users => {
+   *   return serializer.normalizeArrayResponse(instances);
+   * }).then(response => {
+   *   /**
+   *    * [{
+   *    *   firstName: "foo",
+   *    *   lastName; "bar"
+   *    * }]
+   * });
+   * ```
+   */
   normalizeArrayResponse(instances) {
     return Promise.resolve(instances);
   }
 
+  /**
+   * Takes one or several Sequelize intances and returns them.
+   *
+   * @method normalizeResponse
+   * @param {Object} instance
+   * @param {String} method
+   * @return {Promise}<Object, Error>
+   *
+   * @example
+   * ```javascript
+   * return orm.findOne("user", 1).then(user => {
+   *   return serializer.normalizeResponse(user, "findOne");
+   * }).then(response => {
+   *   /**
+   *    * {
+   *    *   firstName: "foo",
+   *    *   lastName; "bar"
+   *    * }
+   * })
+   *
+   * return orm.findAll("user").then(users => {
+   *   return serializer.normalizeResponse(users, "findAll");
+   * }).then(response => {
+   *   /**
+   *    * [{
+   *    *   firstName: "foo",
+   *    *   lastName: "bar"
+   *    * }]
+   * });
+   * ```
+   */
   normalizeResponse(instance, method, fallbackName) {
     switch (method) {
       case "createRecord":
@@ -17,6 +68,26 @@ export default class JSONSerializer {
     }
   }
 
+  /**
+   * Returns a single sequelize instance
+   *
+   * @method normalizeSingularResponse
+   * @param {Object} instance Sequelize model instance
+   * @return {Promise}<Object, Error>
+   *
+   * @example
+   * ```javascript
+   * return orm.findOne("user", 1).then(user => {
+   *   return serializer.normalizeSingularResponse(instance, "findOne");
+   * }).then(response => {
+   *   /**
+   *    * {
+   *    *   firstName: "foo",
+   *    *   lastName; "bar"
+   *    * }
+   * });
+   * ```
+   */
   normalizeSingularResponse(instance) {
     return Promise.resolve(instance);
   }
